c24ba8ba8e7cd790b0e7767c38b43b4f47d5f179
Matt Born <mattborn@users.noreply.github.com>
2025-02-17 15:11:01 -0700

    Fix base href when deployed

--- scripts.js
+++ scripts.js
@@ -1,3 +1,8 @@
+// Set base URL depending on environment
+const base = document.getElementById('base')
+const isGitHubPages = window.location.hostname.includes('github.io') || window.location.hostname === 'mattborn.com'
+base.href = isGitHubPages ? '/alpyne/' : '/'
+
 const g = document.getElementById.bind(document)
 const q = document.querySelectorAll.bind(document)
 
